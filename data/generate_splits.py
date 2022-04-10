e1173e21274a1c859a4624fa47554bb2b5db8f33
gkebe <56805919+gkebe@users.noreply.github.com>
2022-04-10 14:48:10 -0400

    Added alfred_process scripts

---
+++ data/generate_splits.py
@@ -0,0 +1,90 @@
+#
+#
+#
+#
+#
+#
+
+import json
+import random
+import argparse
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument('in_file', type=str, help='a jsonl file of completed task annotations')
+parser.add_argument('tests_percent', type=float, help='A number between 0.0 and 1.0')
+parser.add_argument('train_percent', type=float, help='A number between 0.0 and 1.0')
+parser.add_argument('valid_percent', type=float, help='A number between 0.0 and 1.0')
+parser.add_argument('seed', type=float, help='A float to seed the shuffle. Default is 0.5', 
+                    default=0.5)
+parser.add_argument('out_file', type=str, help='File to write output json to')
+args = parser.parse_args()
+
+
+if args.tests_percent + args.train_percent + args.valid_percent > 1.0:
+    print("Invalid split breakdowns, total is more than 100%")
+    sys.exit(1)
+
+
+parent_dict = {}
+task_list = []
+task_dict = {}
+
+#Open input file and reformat data into {idx, task}
+with open(args.in_file) as src_file:
+    for line in src_file:
+        tmp_dict = json.loads(line)
+        new_dict = {}
+        new_dict['repeat_idx'] = 0
+        #Remove metadata
+        fmtd_task = tmp_dict['task'][:tmp_dict['task'][:(tmp_dict['task'].rfind('-'))].rfind('-')]
+        tmp = fmtd_task.replace("_trial", "/trial")
+        fmtd_task = tmp
+        new_dict['task'] = fmtd_task
+        if fmtd_task not in task_list:
+            task_list.append(fmtd_task)
+            task_dict.update({fmtd_task : []})
+        while new_dict in task_dict[fmtd_task]:
+            new_dict['repeat_idx'] += 1
+        task_dict[fmtd_task].append(new_dict)
+src_file.close()
+
+#Shuffle list in place using seed
+random.seed(args.seed)
+random.shuffle(task_list)
+
+#Generate splits based on input percents
+length = len(task_list)
+breakOne = int(length * float(args.tests_percent))
+breakTwo = int(length * (float(args.tests_percent) + float(args.train_percent)))
+test_splits_a = task_list[0:breakOne]
+train_splits_a = task_list[breakOne:breakTwo]
+validate_splits_a = task_list[breakTwo:]
+
+test_splits = []
+train_splits = []
+valid_splits = []
+
+for task in test_splits_a:
+    for item in task_dict[task]:
+        test_splits.append(item)
+for task in train_splits_a:
+    for item in task_dict[task]:
+        train_splits.append(item)
+for task in validate_splits_a:
+    for item in task_dict[task]:
+        valid_splits.append(item)
+
+#Reformat test_splits to remove task name data
+#for entry in test_splits:
+#    new_task = entry['task'][entry['task'].index('/') + 1 : ]
+#    entry['task'] = new_task
+
+parent_dict["tests"] = test_splits
+parent_dict["train"] = train_splits
+parent_dict["valid"] = valid_splits
+
+
+with open(args.out_file, "w") as target:
+    json.dump(parent_dict, target, indent = 4)
+target.close()
---
+++ data/move_files.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  9 14:20:39 2022
+
+@author: gaoussoukebe
+"""
+
+import shutil
+import json
+import os
+
+# where alfred data is located
+alfred_data_dir = '/nfs/ada/cmat/common/data/json_feat_2.1.0/'
+# where we are putting alfred data
+annotations_dir = '/nfs/ada/cmat/common/data/annotations_2022_04_09/'
+
+transcriptions_json = 'coded_transcripts.jsonl'
+
+splits_json = "splits_out.json"
+
+# if where we are moving data already exists delete it
+if os.path.exists(annotations_dir):
+    shutil.rmtree(annotations_dir)
+
+# create annatations folder
+os.mkdir(annotations_dir)
+
+# Opening JSON file of transcripts
+with open(transcriptions_json, 'r') as json_file:
+    json_list = list(json_file)
+
+# Opening JSON file with splits
+with open(splits_json, 'r') as json_file:
+    splits_dict = json.loads(json_file.read())
+
+# Get all the videos for which there are annatations
+trials = {}
+for json_str in json_list:
+    result = json.loads(json_str)
+    # generate name
+    if '-'.join(result['task'].split('-')[:5]) in trials:
+        trials['-'.join(result['task'].split('-')[:5])].append(result)
+    else:
+        trials['-'.join(result['task'].split('-')[:5])] = [result]
+
+# Mark each video with split
+task_splits = dict()
+for split_name, split_tasks in splits_dict.items():
+    for t in split_tasks:
+        task_splits[t["task"]] = split_name
+
+def copytree(src, dst, symlinks=False, ignore=None):
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        os.makedirs(dst, exist_ok=True)
+        if os.path.isdir(s):
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
+
+counter = 1
+for trial, trial_dicts in trials.items():
+    
+    print(counter, '/', len(trials), ' - ', trial)
+    # generate original path based on which split the video belongs to in the alfred data
+    splits = trial.split('trial')
+    for original_split in ["train", "valid_seen", "valid_unseen", "test_seen", "test_unseen"]:
+        original_path = alfred_data_dir + original_split + "/" + \
+            splits[0][:len(splits[0])-1] + '/trial' + splits[1]
+        if os.path.exists(alfred_data_dir + original_split + "/" + \
+            splits[0][:len(splits[0])-1] + '/trial' + splits[1]):
+            break
+        
+    trial_path = splits[0][:len(splits[0])-1] + '/trial' + splits[1]
+    new_split = task_splits[trial_path]
+    if os.path.exists(annotations_dir + "/" + new_split + "/" + trial_path):
+        continue
+    # create folder for trial based on our splits
+    new_path = annotations_dir + "/" + new_split + "/" + trial_path
+    copytree(original_path, new_path)
+    
+    # make a copy of the original alfred annotations
+    shutil.copy(new_path +"/"+ "traj_data.json", new_path +"/"+ "alfred_traj_data.json")
+    
+    # edit the original alfred traj_data.json to include transcriptions of our annotations instead of Alfred's
+    with open(new_path +"/"+ "traj_data.json", "r") as infile:
+        traj = json.load(infile)
+    
+    our_anns = []
+    for trial_dict in trial_dicts:
+        trial_ann = {'assignment_id': "_".join(trial_dict["task"].split("-")[-2:]),
+                     'high_descs': trial_dict["step_transcriptions"],
+                     'task_desc': trial_dict["summary_transcription"],
+                     'task_intent': trial_dict["intention_transcription"],
+                     'worker_id':trial_dict["worker_id"]}
+        our_anns.append(trial_ann)
+    
+    traj["turk_annotations"]["anns"] = our_anns
+    with open(new_path +"/"+ "traj_data.json", 'w') as outfile:
+        json.dump(traj, outfile, indent=4)
+    
+        counter += 1
+        
+print('Done')
