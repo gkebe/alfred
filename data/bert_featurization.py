67402d65bc7a7ca4f2112586d27a95f9b46f50f3
gkebe <56805919+gkebe@users.noreply.github.com>
2022-04-18 03:01:45 -0400

    Create bert_featurization.py

---
+++ data/bert_featurization.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  9 14:20:39 2022
+
+@author: gaoussoukebe
+"""
+
+from tqdm import tqdm
+import numpy as np
+import torch
+import flair
+import pickle
+import argparse
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--data_dir', type=str, help='directory where annotations are saved', required=True)
+parser.add_argument('--splits_json', type=str, help='json file with train/val/test splits', required=True)
+args = parser.parse_args()
+
+def setup_device(gpu_num=0):
+    """Setup device."""
+    device_name = 'cuda:'+str(gpu_num) if torch.cuda.is_available() else 'cpu'  # Is there a GPU?
+    device = torch.device(device_name)
+    return device
+#astype = lambda x: np.array(x).astype(np.uint8)
+
+
+document_embeddings = flair.embeddings.DocumentPoolEmbeddings([flair.embeddings.BertEmbeddings()])
+
+def proc_sentence(t):
+    sentence = flair.data.Sentence(t, use_tokenizer=True)
+    document_embeddings.embed(sentence)
+    return sentence.get_embedding()
+
+features = {}
+
+# Opening JSON file with splits
+with open(splits_json, 'r') as json_file:
+    splits_dict = json.loads(json_file.read())
+
+for split, ann_list in splits_dict.items():
+    for ann in ann_list:
+        with open(os.path.join(args.data_dir, ann["task"], "pp", f"ann_{ann['repeat_idx']}.json"), 'r') as ann_file:
+            ann_json = json.loads(ann_file.read())
+            ann_dict = ann_json["turk_annotations"]["anns"][ann_json["repeat_idx"]]
+        ann_features = {}
+        ann_features["lang_goal"] = proc_sentence(ann_dict["task_desc"]).detach().to('cpu')
+        ann_features["task_intent"] = proc_sentence(ann_dict["task_intent"]).detach().to('cpu')
+
+        ann_features["lang_instr"] = [proc_sentence(step).detach().to('cpu') for step in ann_dict["high_descs"]]
+
+        features[ann["task"]][f"ann_{ann['repeat_idx']}.json"] = ann_features
+
+
+with open(os.path.join(args.data_dir, "bert_features.pkl"), 'wb') as f:
+    pickle.dump(features, f)
