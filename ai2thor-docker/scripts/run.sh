ef3dc8d33ef85866c7f999272b9094ab3df936d5
kebegaoussou <mb88814@umbc.edu>
2022-04-19 23:47:42 -0400

    added extra -v to run.sh

--- ai2thor-docker/scripts/run.sh
+++ ai2thor-docker/scripts/run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 ALFREDPATH="$( cd -- "$(dirname "$0")"/../.. >/dev/null 2>&1 ; pwd -P )"
 echo $ALFREDPATH
-docker run --rm -it --privileged -v $ALFREDPATH:/home/$USER/alfred -v ~/.ssh:/home/$USER/.ssh -v ~/.torch:/home/$USER/.torch ai2thor-docker bash --user $USER
+if [ -z $1 ];
+then
+    EXTRA=""
+else
+    EXTRA="-v $1"
+fi
+docker run --rm -it --privileged -v $ALFREDPATH:/home/$USER/alfred -v ~/.ssh:/home/$USER/.ssh -v ~/.torch:/home/$USER/.torch $EXTRA ai2thor-docker bash --user $USER
