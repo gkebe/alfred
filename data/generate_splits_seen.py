e09ed916fdace7c0f193788721bd4099c1214ca0
gkebe <56805919+gkebe@users.noreply.github.com>
2022-04-24 00:26:14 -0400

    Update generate_splits_seen.py

--- data/generate_splits_seen.py
+++ data/generate_splits_seen.py
@@ -6,10 +6,6 @@ import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--alfred_data_dir', type=str, help='json_feat_2.1.0 directory from the original Alfred data', required=True)
-parser.add_argument('--in_file', type=str, help='a jsonl file of completed task annotations')
-parser.add_argument('--train_percent', type=float, help='A number between 0.0 and 1.0')
-parser.add_argument('--seed', type=float, help='A float to seed the shuffle. Default is 0.5',
-                    default=0.5)
 parser.add_argument('--out_file', type=str, help='File to write output json to')
 args = parser.parse_args()
 
@@ -33,3 +29,6 @@ for original_split in ["tests_seen", "tests_unseen"]:
             traj_data = json.loads(f.read())
             scene = traj_data["scene"]["floor_plan"]
             task_dict[os.path.join(alfred_data_dir, original_split, trial)] = scene
+
+with open(args.out_file, "w") as target:
+    json.dump(task_dict, target, indent = 4)
