bad090f6f225aab850d1a8e4f6bc13175d3cc26d
gkebe <56805919+gkebe@users.noreply.github.com>
2022-05-02 14:43:47 -0400

    Update seq2seq_im_mask.py

--- models/model/seq2seq_im_mask.py
+++ models/model/seq2seq_im_mask.py
@@ -359,7 +359,7 @@ class Module(Base):
             print(neg_subgoal.shape)
 
             t_loss = self.triplet_loss(anchor_lang, pos_subgoal, neg_subgoal)
-            t_loss = t_loss * pad_valid.float()
+            t_loss = t_loss.view(-1) * pad_valid.float()
             t_loss = t_loss.mean()
             losses['triplet_loss'] = t_loss
 
